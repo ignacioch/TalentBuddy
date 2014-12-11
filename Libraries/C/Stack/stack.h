36165f20361c18cb9783c927bf219f42174824cb
IgnacioCh <ign_ch@hotmail.com>
2014-12-11 23:42:54 +0000

    Adding implementation for stacks

---
+++ Libraries/C/Stack/stack.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int empty(struct node *s);
+struct node* push(struct node *s,int data);
+struct node* pop(struct node *s,int *data);
+void init(struct node* s);
+void display(struct node* head);
+
+
+
